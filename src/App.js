bf2ded3de92e6bc8c55a1546b0275bc9a6c9ecdf
Devin Ryan Riota <devinryanriota@gmail.com>
2020-08-20 16:03:18 +0700

    wip

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
---
+++ src/routes/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.css'
+
+class Home extends React.Component {
+  constructor() {
+    
+  }
+
+}
+
+export default Home
