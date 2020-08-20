0542f00b354ba6dd3919d66d85940aaf9ea3286d
Devin Ryan Riota <devinryanriota@gmail.com>
2020-08-20 20:48:38 +0700

    add create menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './header.css'
 import Logo from '../../assets/logo.svg'
 
-import { HOME_PATH } from '../../routes'
+import { HOME_PATH, CREATE_PATH } from '../../routes'
 
 class Header extends React.Component {
 
@@ -13,6 +13,10 @@ class Header extends React.Component {
           <img src = { Logo } className = 'logo' alt = 'book catalog logo'/>
           <p className = 'name'>book catalog</p>
         </a>
+
+        <div>
+          <a href = { CREATE_PATH } className = 'menu-action'>Create</a>
+        </div>
       </header>
     )
   }
