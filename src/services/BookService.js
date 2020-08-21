57f7438c182601eb9a20469a985ad70a5cbce6d5
Devin Ryan Riota <devinryanriota@gmail.com>
2020-08-21 10:22:01 +0700

    add bookservice

---
+++ src/services/BookService.js
@@ -0,0 +1,35 @@
+import axios from 'axios'
+
+const BASE_URL = 'https://devin-react-basic-workshop-project.vercel.app/api'
+
+const BOOKS_PATH = '/books'
+
+let BookService = {}
+
+BookService.get = (callback) => {
+  const PATH = BASE_URL + BOOKS_PATH
+
+  axios.get(PATH, {
+    params: {}
+  })
+  .then((response) => {
+    console.log('response', response)
+    callback(response)
+  })
+}
+
+BookService.create = (title, author, year, callback) => {
+  const PATH = BASE_URL + BOOKS_PATH
+
+  axios.post(PATH, {
+    title: title,
+    author: author,
+    year: year
+  })
+  .then((response) => {
+    console.log('response', response)
+    callback(response)
+  })
+}
+
+export default BookService
